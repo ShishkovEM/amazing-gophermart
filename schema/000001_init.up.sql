bba75b002b1aac2d1cc50183eb2918d75bbc2e59
Evgenii Shishkov <e.m.shishkov@yandex.ru>
2023-03-25 09:30:51 +0400

    orders and withdrawal tables merged into operations table

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -9,28 +9,19 @@ CREATE TABLE IF NOT EXISTS users (
     CONSTRAINT username_unique UNIQUE (username)
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS operations (
     user_id UUID,
-    order_num VARCHAR(255),
+    operation_num VARCHAR(255),
     accrual FLOAT4,
     status VARCHAR(255),
+    withdrawal FLOAT4,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    CONSTRAINT orders_pkey PRIMARY KEY (order_num),
-    CONSTRAINT orders_users_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
+    withdrawn_at TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT operations_pkey PRIMARY KEY (operation_num),
+    CONSTRAINT operation_users_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS withdrawals (
-    user_id UUID,
-    order_num VARCHAR(255),
-    withdraw FLOAT4,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    CONSTRAINT withdrawals_pkey PRIMARY KEY (order_num),
-    CONSTRAINT withdrawals_users_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
-);
-
-DELETE FROM withdrawals WHERE user_id IN (SELECT user_id FROM users WHERE username LIKE 'test%');
-
-DELETE FROM orders WHERE user_id IN (SELECT user_id FROM users WHERE username LIKE 'test%');
+DELETE FROM operations WHERE user_id IN (SELECT user_id FROM users WHERE username LIKE 'test%');
 
 DELETE FROM users WHERE username LIKE 'test%';
 
@@ -41,16 +32,16 @@ OR REPLACE VIEW balance AS (
       user_id,
       SUM(accrual) total
     FROM
-      orders
+      operations
     GROUP BY
       user_id
   ),
   withdraw AS (
     SELECT
       user_id,
-      SUM(withdraw) withdraw
+      SUM(withdrawal) withdraw
     FROM
-      withdrawals
+      operations
     GROUP BY
       user_id
   )
